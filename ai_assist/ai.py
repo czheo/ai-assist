855420b8adcc3a57a5bf2a1e269713c903e4add0
czheo <1692400+czheo@users.noreply.github.com>
2023-04-11 01:26:19 -0700

    update

--- ai_assist/ai.py
+++ ai_assist/ai.py
@@ -12,28 +12,32 @@ from langchain.utilities import BashProcess
 from dotenv import load_dotenv
 
 
+GOOGLE_SEARCH_LIMIT = 10
+MEMORY_MAX_TOKEN_LIMIT = 300
+
+
 def setup():
     load_dotenv(os.path.expanduser("~/.env"))
-    search = GoogleSearchAPIWrapper(k=10)
+    search = GoogleSearchAPIWrapper(k=GOOGLE_SEARCH_LIMIT)
     bash = BashProcess()
     tools_llm = OpenAI()
-    memory = ConversationSummaryBufferMemory(llm=tools_llm, max_token_limit=100, memory_key="chat_history", return_messages=True)
+    memory = ConversationSummaryBufferMemory(llm=tools_llm, max_token_limit=MEMORY_MAX_TOKEN_LIMIT, memory_key="chat_history", return_messages=True)
     tools = load_tools(["llm-math"], llm=tools_llm)
     tools.extend([
         Tool(
             name="google search",
             func=search.run,
-            description="useful when you need to ask with search"
+            description="useful for when you need to answer questions about current events or the current state of the world. the input should be a single search term."
         ),
         PythonREPLTool(),
         Tool(
             name="bash",
             func=bash.run,
-            description="useful when you need to run a shell command to interact with your local machine. the raw output of the command will be returned."
+            description="useful when you need to run a shell command to interact with the local machine. the input should be a bash command. the raw output of the command will be returned."
         ),
     ])
 
-    llm = ChatOpenAI(temperature=0.9)
+    llm = ChatOpenAI(temperature=0.7)
     return initialize_agent(tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory)
 
 
